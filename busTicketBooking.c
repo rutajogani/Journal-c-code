ea0373b01ade0900eaf4a0a487e083c8a28e90b2
Ruta Jogani <ruta007jogani@gmail.com>
2025-11-27 19:00:09 +0530

    add: code in bus file

--- busTicketBooking.c
+++ busTicketBooking.c
@@ -8,7 +8,7 @@ int bookSeat()
     printf("How Many seat do you want: ");
     scanf("%d", &seat);
     count = 0;
-    
+
     for (int row = 0; row < 6; row++)
     {
         for (int column = 0; column < 4; column++)
@@ -18,14 +18,19 @@ int bookSeat()
                 seats[row][column] = 1;
                 count++;
             }
-            if (count==seat)
+            if (count == seat)
             {
                 printf("Your seat is Booked\n");
                 printf("**-----------------------------------**\n");
                 return 0;
             }
-            
         }
+    }
+    if (count > seat)
+    {
+        printf("Seat is Not Available\n");
+        printf("**-----------------------------------**\n");
+        return 0;
     }
 }
 int showBus()
@@ -42,20 +47,19 @@ int showBus()
 int main()
 {
     int ticketBooking;
-    
+
     for (int i = 0;; i++)
     {
         printf("**-----------------------------------**\n");
         printf("Do you want Book Ticket so Enter - 1\n");
         printf("Do you want Show Seat so Enter - 2\n");
         printf("Do you want Exit so Enter - 3\n");
-        
+
         printf("**-----------------------------------**\n");
         printf("What do you want?\n");
         printf("**-----------------------------------**\n");
 
         scanf("%d", &ticketBooking);
-
 
         switch (ticketBooking)
         {
@@ -68,7 +72,7 @@ int main()
         case 3:
             return 0;
         default:
-            printf("Not a Optional");
+            printf("Not a Optional\n");
             break;
         }
     }
